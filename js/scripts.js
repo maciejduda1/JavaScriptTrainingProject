13e9b71d201cd0935a3e6ae46c6d1d53af74493a
Maciej Duda <maciej.duda1@gazeta.pl>
2017-10-26 19:45:08 +0200

    konsolowy kalkulator pola trójkąta

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,5 @@
-﻿var name = prompt ('Enter your name');
-//komentarz nr 1 liniowy
+﻿var a = prompt('podaj długość boku trójtkąta: '),
+	h = prompt('podaj wysokość trójtkąta: '),
+	triangleArea = a*h/2;
 
-//alert ('Siemandero, ' + name); zakomentowana linia z poleceniem alert
-
-//komentarz nr 2 liniowy
-console.log('Hello, ' + name);
-/* Długi komentarz blokowy
-1 linia komentarza
-2 linia komentarza
-*/
+console.log('Triangle field with base a: ' + a + ' and height placed on this side - h: ' + h + 'is equal to: ' + triangleArea );
