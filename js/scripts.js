0db335abca92956638d27396355b6cbf3497d78e
Maciej Duda <maciej.duda1@gazeta.pl>
2017-11-01 12:06:14 +0100

    Funkcja licząca pole trójkąta

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-﻿
+﻿var a;
+var h;
+
+function getTriangleArea(a, h){
+	var triangleArea;
+	if ((a < 0) || (h < 0)) {
+		alert('Nieprawidłowe wartości!');
+	}
+	else {
+		triangleArea = a * h / 2;
+		return triangleArea;
+	}
+}
+console.log(getTriangleArea(10, 6));
+
+var triangle1Area = getTriangleArea(15,8);
+var triangle2Area = getTriangleArea(8,12);
+var triangle3Area = getTriangleArea(-2, 5);
+
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
