dfeb7bfba38eb3f81c7f656cd31470493e186fe7
Maciej Duda <maciej.duda1@gazeta.pl>
2017-11-06 18:02:20 +0100

    zadanie9.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-﻿
+﻿var pageButton = document.getElementsByClassName('button');
+// console.log(pageButton.length); - wyświetla ilość elementów z klasą button (3)
+// console.log(pageButton); - wyświetla tablicę z 3 elementami <button.button>
+
+for(i = 0; i != pageButton.length; i++) {
+	console.log(pageButton[i].innerText);
+}
+// Powyżej, moja droga dochodzenia do rozwiązania ;) 
