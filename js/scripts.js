48240a10151d119b66f669c1e56d399301d43b71
Maciej Duda <maciej.duda1@gazeta.pl>
2017-11-07 17:55:55 +0100

    kolorek na parzyste

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,9 @@ var list = document.getElementById('list');
 add.addEventListener('click', function(){	
 	var element = document.createElement('li');
 	var number = document.getElementsByTagName('li').length;
+	if (number % 2 === 0){
+		element.style.color = 'red';
+	}
 	element.innerHTML = 'item ' + number;
 	list.appendChild(element);
 });
