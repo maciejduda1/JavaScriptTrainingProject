1401c0459ed0ac151faaf201efe5dbe3d8d6d8bf
Maciej Duda <maciej.duda1@gazeta.pl>
2017-11-07 11:07:41 +0100

    zadanie9.6 Eventy

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-﻿
+﻿var add = document.getElementById('addElem');
+var list = document.getElementById('list');
+
+add.addEventListener('click', function(){	
+	var element = document.createElement('li');
+	var number = document.getElementsByTagName('li').length;
+	element.innerHTML = 'item ' + number;
+	list.appendChild(element);
+});
+
