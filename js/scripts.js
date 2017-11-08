1f50bdb915b75ae014a0abbd737b684991013ff3
Maciej Duda <maciej.duda1@gazeta.pl>
2017-11-08 16:32:26 +0100

    jquery 1-wsze starcie

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-﻿
+﻿$(function(){
+	console.log('DOM loaded');
+});
+
+$('span').each(function(index, element){
+		if (index % 2 == 0) {
+			$(element).css('color' , 'red');
+		}
+});
+
+$('p').each(function(index, element){
+	$(element).append('<button class = "btn" data-tmp="' + index + '">Click me</button>');  
+});
+
+$('button').click(function(){
+	alert($(this).attr("data-tmp"));
+});
