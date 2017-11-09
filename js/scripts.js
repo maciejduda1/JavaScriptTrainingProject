9b9d52baf2a196e103e3c9f43ab03d4a7a625fb3
Maciej Duda <maciej.duda1@gazeta.pl>
2017-11-09 14:59:58 +0100

    Szukam rozwiązania

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,13 @@
 	console.log('DOM loaded');
 });
 
+/*
 $('span').each(function(index, element){
 		if (index % 2 == 0) {
 			$(element).css('color' , 'red');
 		}
 });
+*/
 
 $('p').each(function(index, element){
 	$(element).append('<button class = "btn" data-tmp="' + index + '">Click me</button>');  
@@ -15,3 +17,22 @@ $('p').each(function(index, element){
 $('button').click(function(){
 	alert($(this).attr("data-tmp"));
 });
+
+//nowe działanie :> 
+
+$('span').each(function(index, element){
+	$(element).attr('id' , "special" + index);		// ten sam efekt osiągnąłem dając funkcji jedynie paramter index, a $(element) zastępując $(this) :> 
+});
+
+var ignored = [$("#special1"), $("#special2")];          
+console.log(ignored[0]);
+
+var allSpan = $('span');
+console.log(allSpan);
+
+
+var diff = $(allSpan).not(ignored).get();
+console.log(diff);
+
+$(diff).css('color' , 'red');
+
