3a550f76a1be10bcc36ec79076daa1cac14ea182
Maciej Duda <maciej.duda1@gazeta.pl>
2017-11-09 15:19:45 +0100

    Karuzela

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,18 @@
 	//this code will execute after the DOM is loaded
 });
 
-var carouselList = $("#carousel ul")
+var carouselList = $("#carousel ul");
+setInterval(changeSlide, 5000);
+
+
+function changeSlide(){
+	carouselList.animate({'marginLeft':-800}, 3000, moveFirstSlide);
+}
+
+
+function moveFirstSlide(){
+	var firstItem = carouselList.find("li:first");
+	var lastItem = carouselList.find("li:last");
+	lastItem.after(firstItem);
+	carouselList.css({marginLeft:0});
+}
