33cd6e79812f75327d27cb06f4ff9a92975a6d19
Maciej Duda <maciej.duda1@gazeta.pl>
2017-11-11 23:26:31 +0100

    dalsze poprawki karuzeli!

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,27 @@
 ﻿$(function(){
 	//this code will execute after the DOM is loaded
 });
-
+var carouselChangeTimer;
 var carouselList = $("#carousel ul");
-var intervalSetup = setInterval(changeSlide, 8000);
+
+function intervalSetup(changeTimerNow){
+	if (changeTimerNow == 'Now'){
+		clearInterval(carouselChangeTimer);
+	return	setInterval(changeSlide, 8000);
+	}
+	else {
+	return setInterval(changeSlide, 8000);
+	}
+}
 
 
 function changeSlide(){
 	carouselList.animate({'marginLeft':-400}, 3000, moveFirstSlide);
 }
 
+function changeSlideOtherDirection(){
+	carouselList.animate({'marginLeft': 0}, 3000, moveLastSlide);
+}
 
 function moveFirstSlide(){
 	var firstItem = carouselList.find("li:first");
@@ -18,21 +30,26 @@ function moveFirstSlide(){
 	carouselList.css({marginLeft:0});
 }
 
-$("#leftArrow").click(function(){
-	changeSlide();
-	clearInterval(intervalSetup);
-	return intervalSetup = setInterval(changeSlide, 8000);
-});
-
-$("#rightArrow").click(function(){
-	moveLastSlide();
-	clearInterval(intervalSetup);
-	return intervalSetup = setInterval(changeSlide, 8000);
-})
-
 function moveLastSlide(){
 	var firstItem = carouselList.find("li:first");
 	var lastItem = carouselList.find("li:last");
-	lastItem.before(firstItem);
+	firstItem.before(lastItem);
 	carouselList.css({marginLeft:-400});
 }
+
+$("#rightArrow").click(function(){							
+	changeSlide();
+	carouselChangeTimer = intervalSetup('Now');
+});
+
+$("#leftArrow").click(function(){
+	changeSlideOtherDirection();
+	carouselChangeTimer = intervalSetup('Now');
+});
+
+
+
+
+// Karuzela:
+carouselChangeTimer = intervalSetup();
+
