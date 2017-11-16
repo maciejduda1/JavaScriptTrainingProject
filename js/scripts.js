19f788818407e7ca56f892519398972e97cc8f11
Maciej Duda <maciej.duda1@gazeta.pl>
2017-11-16 16:24:29 +0100

    Dodatkowa gwaranacja na dwa sposoby! wewnątrz i na zewnątrz klasy

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,23 @@
 	this.price = price;
 	this.color = color;
 	this.oes = oes;
+	this.getWarrantyCost = function(){
+		return price * 0.1;
+	};
+	this.extVarrancy = price * 0.1; 
+
 }
 
 Phone.prototype.printInfo = function(){
-	console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + '.'); 
+	console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + '.' + 'the cost of extended varrancy is ' + this.getWarrantyCost()); 
 };
 
 Phone.prototype.oesVersion = function(){
 	console.log('The oes of ' + this.brand + ' is: ' + this.oes);
+};
+
+Phone.prototype.extendedVarrancyPrice = function(){
+	console.log('the extended Varrancy Price of this phone is: ' + this.extVarrancy);
 };
 
 var samsungGalaxyS6 = new Phone("Samsung", 1500, "black", "Android");
@@ -19,5 +28,6 @@ var onePlusOne = new Phone("加科技", 800, "yellow", "Android");
 
 iPhone6S.printInfo();
 iPhone6S.oesVersion();
+iPhone6S.extendedVarrancyPrice();
 samsungGalaxyS6.printInfo();
 onePlusOne.printInfo();
