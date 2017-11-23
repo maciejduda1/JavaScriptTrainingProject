c3d46418adb5b203df4cf8b0c1a0e8a25d3a2f59
Maciej Duda <maciej.duda1@gazeta.pl>
2017-11-23 18:31:56 +0100

    chuck norris AJAX joke

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-﻿
+﻿var url = 'http://api.icndb.com/jokes/random';
+var button = document.getElementById('get-joke');
+var jokeparagraph = document.getElementById('joke');
+
+button.addEventListener('click', function(){
+	getJoke();
+});
+
+function getJoke(){
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url);
+	xhr.addEventListener('load', function(){
+		var response = JSON.parse(xhr.response);
+		jokeparagraph.innerHTML = response.value.joke;
+	});
+	xhr.send();
+}
+
+getJoke();
