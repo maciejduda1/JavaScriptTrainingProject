e5c1ec4fe66f1cd30871836d2ff288f03309c66c
Maciej Duda <maciej.duda1@gazeta.pl>
2017-11-29 12:52:03 +0100

    Dużo pracy... przynajmniej działa

--- js/scripts.js
+++ js/scripts.js
@@ -5,20 +5,58 @@ var countriesList = $('#countries'); //<ul>
 $('#search').click(searchCountries);
 
 function searchCountries() {
-	var countryName = $('#country-name').val(); //input ; val() - Get the current value of the first element in the set of matched elements or set the value of every matched element.
-
+	$('#full-info').css("display", "none");
+	var countryName = $('#country-name').val(); 
 	if (!countryName.length) countryName = 	'Poland';
 
 	$.ajax({
 		url: url + countryName,
 		method: 'GET',
-		success: showCountriesList
+		success: showCountriesList		
 	});
 }
 
 function showCountriesList(resp){
-	countriesList.empty(); // czyści listę krajów po poprzednim zapytaniu
-	resp.forEach(function(item){     // forEach() przejdzie po każdym elemencie resp i wykona finction()
-		$('<li>').text(item.name).appendTo(countriesList);
-	});
+	var countryTable = resp;
+	countriesList.empty();
+	resp.forEach(function(item){
+		$('<button>').addClass(item.name).text(item.name).appendTo($('<li>').appendTo(countriesList));
+	});	
+	
+	$('button').click(function(){
+		var buttonClass = $(this).attr("class");
+		var x = countryTable.filter(function(itemsToSearch){
+			return itemsToSearch.name == buttonClass;
+		});
+		displayCountryInfo(x);
+	});	
 }
+
+function displayCountryInfo(whatCountryToShow){
+	var flag = whatCountryToShow[0].flag ;
+	var $capital = $('#capital');
+	var $currency = $('#currency');
+	var $population = $('#population');
+	var $countryNameFull = $('#country-name-full');
+	
+	$capital.empty();
+	$currency.empty();
+	$population.empty();
+	$('#flag').empty();
+	$countryNameFull.empty();
+
+	$countryNameFull.text(whatCountryToShow[0].name);
+	$('<td>').text('stolica: ').appendTo($capital);
+	$('<td>').text(whatCountryToShow[0].capital).appendTo($capital);
+	$('<td>').text('waluta: ').appendTo($currency);
+	$('<td>').text(whatCountryToShow[0].currencies[0].name).appendTo($currency);
+	$('<td>').text('populacja: ').appendTo($population);
+	$('<td>').text(whatCountryToShow[0].population).appendTo($population);
+	$('#flag').prepend($('<img>', {src: flag, }).css('height', 'inherit').css('width', 'inherit'));
+
+	$('#full-info').css("display", "flex");
+}
+
+
+
+
